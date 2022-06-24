b73b2d76899e9e9dadfb12464d97634a399a51c9
odaLemos <oda.lemos@gmail.com>
2022-06-23 23:58:48 -0300

    change api endpoint consult

--- src/BacklogApi.tsx
+++ src/BacklogApi.tsx
@@ -3,5 +3,5 @@ import axios from "axios"
 const api = axios.create({baseURL:"http://127.0.0.1:5000"})
 
 export default function getBacklogList() {
-    return api.get("/api/v1/backloglist/")
+    return api.get("/api/v1/backlog/")
 }
